bda7dcf225ade95b038f873392c799910a7ab0a2
Leo-Macedo <leonardo.aurieni@etec.sp.gov.br>
2023-09-08 21:50:18 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -4,63 +4,70 @@ const ctx = canvas.getContext('2d');
 
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
+//variaveis
+var moverx = 50;
+document.addEventListener('keydown', function(event){
+    if (event.key === 'd') {
+        moverx += 10;
+    }
+
+});
 
 //bone
 ctx.fillStyle = 'white';
 ctx.strokeStyle = 'white';
 ctx.lineWidth = 2.5;
 ctx.beginPath();
-ctx.fillRect(50, 37.5, 25, 12.5);
-ctx.moveTo(75,48.5);
-ctx.lineTo(90,48.5);
+ctx.fillRect(moverx , 37.5, 25, 12.5);
+ctx.moveTo(moverx + 25,48.5);
+ctx.lineTo(moverx + 40,48.5);
 ctx.stroke();
 
 //cabeca
 ctx.fillStyle = '#fcde8b';
-ctx.fillRect(50, 50, 25, 17.5);
+ctx.fillRect(moverx , 50, 25, 17.5);
 
 //boca
 ctx.beginPath();
 ctx.strokeStyle = 'black';
-ctx.moveTo(55,62.5);
-ctx.quadraticCurveTo(62.5,67.5,70,62.5);
+ctx.moveTo(moverx + 5,62.5);
+ctx.quadraticCurveTo(moverx + 12.5,67.5,70,62.5);
 ctx.stroke();
 
 //olhos
 ctx.fillStyle = 'black';
 ctx.beginPath();
-ctx.arc(56, 55, 1.5, 0, Math.PI * 2);
+ctx.arc(moverx + 6, 55, 1.5, 0, Math.PI * 2);
 ctx.fill();
 
 ctx.beginPath();
-ctx.arc(69, 55, 1.5, 0, Math.PI * 2);
+ctx.arc(moverx + 19, 55, 1.5, 0, Math.PI * 2);
 ctx.fill();
 
 //braços
 ctx.fillStyle = '#fcde8b';
-ctx.fillRect(75, 67.5, 7.5, 27.5);
-ctx.fillRect(42.5, 67.5, 7.5, 27.5);
+ctx.fillRect(moverx + 25, 67.5, 7.5, 27.5);
+ctx.fillRect(moverx - 7.5, 67.5, 7.5, 27.5);
 
 //camisa
 ctx.fillStyle = '#6210b9';
-ctx.fillRect(50, 67.5, 25, 25);
-ctx.fillRect(75, 67.5, 7.5, 10);
-ctx.fillRect(42.5, 67.5, 7.5, 10);
+ctx.fillRect(moverx , 67.5, 25, 25);
+ctx.fillRect(moverx + 25, 67.5, 7.5, 10);
+ctx.fillRect(moverx - 7.5, 67.5, 7.5, 10);
 
 //shorts
 ctx.fillStyle = 'white';
-ctx.fillRect(50, 92.5, 25, 15);
+ctx.fillRect(moverx, 92.5, 25, 15);
 
 //pernas
 ctx.fillStyle = '#fcde8b';
-ctx.fillRect(52.5, 107.5, 7.5, 17.5);
-ctx.fillRect(65, 107.5, 7.5, 17.5);
+ctx.fillRect(moverx + 2.5, 107.5, 7.5, 17.5);
+ctx.fillRect(moverx + 15, 107.5, 7.5, 17.5);
 
 //sapato
 ctx.fillStyle = '#cccccc';
-ctx.fillRect(52.5, 122.5, 7.5, 7.5);
-ctx.fillRect(65, 122.5, 7.5, 7.5);
-
+ctx.fillRect(moverx + 2.5, 122.5, 7.5, 7.5);
+ctx.fillRect(moverx + 15, 122.5, 7.5, 7.5);
 
 //labirinto
 ctx.beginPath();
